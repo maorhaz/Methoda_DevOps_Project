2044a718a33c65c69d1f26779bfb83169f8b184f
maorhaz <86194007+maorhaz@users.noreply.github.com>
2023-01-16 19:01:42 +0200

    integrate features

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from InactiveRepositoriesScanner.InactiveRepositoriesScanner import InactiveRepositoriesScanner
+from Logger.Logger import Logger
+from RepositoriesRetriever.RepositoriesRetriever import RepositoriesRetriever
+
+
 def main():
-    print('hello world')
+    repository_retriever = RepositoriesRetriever()
+    inactive_repository_scanner = InactiveRepositoriesScanner()
+    logger = Logger()
+
+    repositories = repository_retriever.retrieve()
+    inactive_repos = inactive_repository_scanner.scan(repositories)
+    logger.log(inactive_repos)
 
 
 if __name__ == '__main__':
