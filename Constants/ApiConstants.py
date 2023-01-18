993f340d57834d523b6bd23b152b31758c36af96
maorhaz <86194007+maorhaz@users.noreply.github.com>
2023-01-18 18:08:31 +0200

    Changed workspace_url constant

--- Constants/ApiConstants.py
+++ Constants/ApiConstants.py
@@ -1,9 +1,8 @@
 URL_HEADER = 'https://api.bitbucket.org'
 API_VERSION = '/2.0'
 REPOSITORIES = '/repositories'
-USERNAME = '/maorhazut'
+#USERNAME = '/maorhazut'
 
-WORKSPACE_URL = URL_HEADER + API_VERSION + REPOSITORIES + USERNAME
+WORKSPACE_URL = URL_HEADER + API_VERSION + REPOSITORIES
 
-ACCESS_TOKEN = 'XwZQbs9eTU7Sm8eZXjuSgenH5KFUUuMg'  # todo move to env var
 
