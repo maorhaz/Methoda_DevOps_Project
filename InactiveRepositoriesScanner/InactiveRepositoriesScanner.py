32a721d4af80f7641f842bd2c1dcef85c3b7eb8a
maorhaz <86194007+maorhaz@users.noreply.github.com>
2023-01-18 18:14:35 +0200

    Changed functionallity of scanner

--- InactiveRepositoriesScanner/InactiveRepositoriesScanner.py
+++ InactiveRepositoriesScanner/InactiveRepositoriesScanner.py
@@ -1,6 +1,7 @@
 import datetime
-import json
+from itertools import repeat
 
+import pytz
 from Constants import ApiConstants
 from InactiveRepositoriesScanner.IInactiveRepositoriesScanner import IInactiveRepositoriesScanner
 
@@ -10,20 +11,17 @@ class InactiveRepositoriesScanner(IInactiveRepositoriesScanner):
 
     def scan(self, retrieved_repositories):
         for repository in retrieved_repositories:
-            repository_url = ApiConstants.WORKSPACE_URL + repository['name']  # fixme ??
-            repository_response = requests.get(
-                repository_url.format(workspace_name=repository['workspace']['name'],
-                                      repository_name=repository['name']),
-                auth=('username', ApiConstants.ACCESS_TOKEN))
-            repository_json = json.loads(repository_response.text)
-
-            is_valid = self.validate_date(repository_json)
+            is_valid = self.validate_date(repository)
             if is_valid:
-                self.inactive_repositories.append(repository_json)
+                self.inactive_repositories.append(repository)
+                return self.inactive_repositories
 
     @staticmethod
     def validate_date(repository_json):
+        delta_time = (datetime.datetime.now() - datetime.timedelta(days=365))
+        delta_time = delta_time.replace(tzinfo=pytz.utc)
         last_updated = datetime.datetime.strptime(repository_json['updated_on'], '%Y-%m-%dT%H:%M:%S.%f%z')
-        if datetime.datetime.now() - last_updated > datetime.timedelta(days=365):
+        last_updated = last_updated.replace(tzinfo=pytz.utc)
+        if last_updated < delta_time:
             return True
         return False
